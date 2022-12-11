dce12779d28b69a3166466edc81dc3f04d7eeb7b
Olov <starpurple011@gmail.com>
2022-12-11 08:23:10 -0300

    Terminado a interação com o banco de dados usando o sqlite3, adicionando no arquivo o driver de player music e sqlite

---
+++ src/src/DB.java
@@ -0,0 +1,81 @@
+package src;
+import java.sql.*;
+public class DB {
+ private String pathDB ="jdbc:sqlite:DB.db";
+ private Connection connection;
+ private Statement statement;
+ private ResultSet resultSet;
+ 	public void db(){
+ 		try {
+ 			connection = DriverManager.getConnection(pathDB);
+ 			statement = connection.createStatement();
+ 		}catch(SQLException e) {
+ 			System.out.print("Falha na coneção "+e.getMessage()+e.getErrorCode());
+ 		}
+ 	}
+ 	public void creatTable() {
+ 		db();
+ 		try {
+			statement.execute("DROP TABLE IF EXISTS candidato");
+			statement.execute("CREATE TABLE candidato(num INT PRIMARY KEY,name TEXT NOT NULL,"
+					+ "partido TEXT NOT NULL,image NOT NULL, votos INT DEFAULT '0');");
+		} catch (SQLException e) {
+			System.out.print("Falha ao criar tabela ao Data base"+e.getMessage()+e.getErrorCode());
+		}
+ 	}
+ 	public void add(int numero, String name,String pathImage, String partido) {
+ 		db();
+ 		try {
+ 			String insert = "INSERT INTO candidato(num,name,partido,image)"
+ 					+" VALUES "+"("+numero+","+"\'"+name+"\'"+","+"\'"
+ 					+pathImage+"\'"+","+"\'"+partido+"\'"+")";
+ 			statement.executeUpdate(insert);
+		} catch (SQLException e) {
+			System.out.print("Falha ao incrementar dado ao Data base"+e.getMessage()+e.getErrorCode());
+		}
+ 	}
+ 	public void fromVotosALL() {
+ 		db();
+        try {
+        	resultSet = statement.executeQuery("SELECT sum(votos) FROM candidato");
+			System.out.print(resultSet.getInt("sum(votos)"));
+		} catch (SQLException e) {
+			System.out.print("Falha ao ler dados do Data base"+e.getMessage()+e.getErrorCode());
+		}
+ 	}
+ 	public void fromVotosCandidato(int num)  {
+ 		db();
+ 		try {
+			resultSet = statement.executeQuery("SELECT votos FROM candidato WHERE num="+num);
+			System.out.print(resultSet.getInt("votos"));
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+ 	}
+ 	public void insertVotos(int votos,int numero) {
+ 		db();
+ 		try {
+ 			String update = "UPDATE candidato SET votos= "+votos+" WHERE num = "+numero;
+ 			statement.executeUpdate(update);
+		} catch (SQLException e) {
+		}
+ 	}
+ 	public void editCandidato(int numeroOld, int numeroNew, String nameNew, String partidoNew, String pathImageNew) {
+ 		db();
+ 		try {
+ 			String update = "UPDATE candidato SET num ="+numeroNew+",nameNew ="+"\'"+nameNew+"\'"+",partidoNew ="
+ 					+"\'"+partidoNew+"\'"+",image = "+"\'"+pathImageNew+"\'"+" WHERE num = "+numeroOld;
+ 			statement.executeUpdate(update);
+		} catch (SQLException e) {
+		}
+ 	}
+ 	public void deleteCandidato(int numero) {
+ 		db();
+ 		try {
+ 			String delete = "DELETE FROM candidato WHERE num="+numero;
+			statement.execute(delete);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+ 	}
+}
--- src/src/Main.java
+++ src/src/Main.java
@@ -4,7 +4,6 @@ public class Main {
 
 	public static void main(String[] args) {
 		Gui janeça = new Gui();
-
 	}
 
 }
